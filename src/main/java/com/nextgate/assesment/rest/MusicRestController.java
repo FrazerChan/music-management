400ae101b87aea15c0e6d7b6ebe6550a4348c27f
Frazer Chan <frazer@pop-os.localdomain>
2021-02-10 14:24:08 +0000

    Add get all and add record rest endpoints

--- src/main/java/com/nextgate/assesment/rest/MusicRestController.java
+++ src/main/java/com/nextgate/assesment/rest/MusicRestController.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.nextgate.assesment.models.Singer;
+import com.nextgate.assesment.models.Album;
+import com.nextgate.assesment.models.User;
 
 /**
  * A REST controller for managing the music catalogue via the Music service
@@ -19,6 +26,36 @@ public class MusicRestController {
     
     @Autowired
     private MusicService musicSerivce;
+
+    @GetMapping("singers")
+    public Iterable<Singer> singers() {
+        return musicSerivce.getAllSingers();
+    }
+
+    @PostMapping(value="add_singer")
+    public @ResponseBody Singer addNewSinger (@RequestBody Singer singer) {
+        return musicSerivce.addSinger(singer);
+    }
+
+    @GetMapping("albums")
+    public Iterable<Album> albums() {
+        return musicSerivce.getAllAlbums();
+    }
+
+    @PostMapping(value="add_album")
+    public @ResponseBody Album addNewAlbum (@RequestBody Album album) {
+        return musicSerivce.addAlbum(album);
+    }
+
+    @GetMapping("users")
+    public Iterable<User> users() {
+        return musicSerivce.getAllUsers();
+    }
+
+    @PostMapping(value="add_user")
+    public @ResponseBody User addNewUser (@RequestBody User user) {
+        return musicSerivce.addUser(user);
+    }
     
     /**
      * GET method to retrieve a song via an Id
--- src/main/java/com/nextgate/assesment/service/MusicService.java
+++ src/main/java/com/nextgate/assesment/service/MusicService.java
@@ -2,6 +2,14 @@ package com.nextgate.assesment.service;
 
 import com.nextgate.assesment.datatypes.Song;
 import org.springframework.stereotype.Service;
+import com.nextgate.assesment.models.Singer;
+import com.nextgate.assesment.repositories.SingerRepository;
+import com.nextgate.assesment.models.Album;
+import com.nextgate.assesment.repositories.AlbumRepository;
+import com.nextgate.assesment.models.User;
+import com.nextgate.assesment.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 /**
  * The Music service that will handle the CRUD operations
@@ -13,6 +21,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MusicService {
 
+    @Autowired
+    private SingerRepository singerRepository;
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     /**
      * Retrieve a song by Id
      *
@@ -21,6 +38,30 @@ public class MusicService {
      */
     public Song getSongById(String Id) {
         return new Song("Test Song");
+    }
+
+    public Iterable<Singer> getAllSingers() {
+        return singerRepository.findAll();
+    }
+
+    public Singer addSinger(Singer singer){
+        return singerRepository.save(singer);
+    }
+
+    public Iterable<Album> getAllAlbums() {
+        return albumRepository.findAll();
+    }
+
+    public Album addAlbum(Album album){
+        return albumRepository.save(album);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user){
+        return userRepository.save(user);
     }
 
 }
